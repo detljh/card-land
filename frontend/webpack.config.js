853d692b8ced276ff65350e6cf8cd2508aac8d38
lisa <lisa.ng8@hotmail.com>
2020-04-08 18:54:58 +1000

    moved webpack to deps

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-          title: 'Card Land',
+          title: 'Minigames online',
           template: 'public/index.html',
         })
     ],
