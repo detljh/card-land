ddb9da55cb5e71e3549959c44ced514070f69de7
lisa <lisa.ng8@hotmail.com>
2020-04-20 15:57:17 +1000

    page transition fade on enter

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -5,16 +5,29 @@ import LoadingRoom from './loading_room';
 import TicTacToe from './tic_tac_toe';
 import history from '../history';
 import gameTypes from '../../../constants/gameTypes';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './transitions.scss';
 
 class App extends React.Component {
     render() {
         return (
             <Router history={history}>
-                <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route path='/room' component={LoadingRoom} />
-                    <Route path={'/' + gameTypes.TIC_TAC_TOE} component={TicTacToe} />
-                </Switch>
+                <Route render={({ location }) => (
+                    <TransitionGroup>
+                        <CSSTransition
+                        key={location.pathname}
+                        timeout={{enter: 500}}
+                        classNames="page">
+                            <div>
+                                <Switch location={location}>
+                                    <Route exact path='/' component={Home} />
+                                    <Route path='/room' component={LoadingRoom} />
+                                    <Route path={'/' + gameTypes.TIC_TAC_TOE} component={TicTacToe} />
+                                </Switch>
+                            </div>
+                        </CSSTransition>
+                    </TransitionGroup>
+                )} />
             </Router>
         )
     }
